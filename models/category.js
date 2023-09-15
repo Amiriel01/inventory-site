52823ea1482a10067ad27f1b94e5534b316286f9
Sarah Quinney <quinney.sarah@gmail.com>
2023-09-14 19:21:54 -0500

    created category model

--- models/category.js
+++ models/category.js
@@ -1,8 +1,11 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// const CatagorySchema = new Schema({
-//     category_name: { type: String, require: true, maxLength: 50 },
+const CatagorySchema = new Schema({
+    category_name: { type: String, require: true, maxLength: 50 },
+})
 
-// })
+CatagorySchema.virtual("url").get(function() {
+    return `/category/${this._id}`;
+})
