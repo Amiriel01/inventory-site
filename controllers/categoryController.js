8bdf62a81718788bfe83bc6c9acf9b00f6d8fd75
Sarah Quinney <quinney.sarah@gmail.com>
2023-09-21 19:54:24 -0500

    category_form created, routes from category page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,12 +3,12 @@ const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 // const CategoryInstance = require("../models/categoryinstance");
 const Item = require("../models/item");
+const { body, validationResult } = require("express-validator");
 
 
 //Display a list of all categories//
 exports.category_list = asyncHandler(async (req, res, next) => {
     const categoryInstances = await Category.find().populate().exec()
-    //     //render category list//
     res.render("category_list", {
         category_list: categoryInstances
     });
@@ -38,14 +38,53 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 
 
 //Display Category create form on GET//
-exports.category_create_get = asyncHandler(async (req, res, next) => {
-    res.send("Not Created Yet: Category Create GET");
-});
+exports.category_create_get = (req, res, next) => {
+    res.render("category_form", { title: "Create New Category"});
+};
 
 //Handle Category create on POST//
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-    res.send("Not Created Yet: Category Create Post")
-});
+exports.category_create_post = [
+    //validate and sanitize the category_name field//
+    body("category_name", 'Tea category is blank.')
+    //trim so there is no white space around the name//
+    .trim()
+    //check to make sure it is at least 5 characters//
+    .isLength({ min: 1 })
+    .escape(),
+
+    //process request after validation and sanitation//
+    asyncHandler(async (req, res, next) => {
+        //extract the validation errors from the request//
+        const errors = validationResult(req);
+
+        //create tea category object with escaped and trimmed information//
+        const category = new Category({ category_name: req.body.category_name });
+
+        if (!errors.isEmpty()) {
+            //If there are errors, render the form again with the sanitized values/error messages.
+            res.render("category_form", {
+                title: "Create New Category",
+                category: category,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            //Form data is valid. Check to be sure the tea category does not already exist//
+            const categoryExists = await Category.findOne({ category_name: req.body.category_name })
+            //use collation to check for letter case matches so that words with capitals and lower cases don't create duplicates//
+            .collation({ locale: "en", strength: 2 })
+            .exec();
+            if (categoryExists) {
+                //category already exists, redirect to it's detail page//
+                res.redirect(categoryExists.url);
+            } else {
+                await category.save();
+                //after category is saved, redirect to category page//
+                res.redirect(category.url);
+            }
+        }
+    }),
+]
 
 //Display Category delete form on GET//
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
